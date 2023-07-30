a60c561d827fb6f23845c3d702b3bd86822c4781
kprze <kacperej@gmail.com>
2023-07-30 13:54:25 +0100

    Add and call game function

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+game();
+
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (i = 0; i < 5; i++){
+        let computerChoice = getComputerChoice();
+        let playerChoice = getPlayerChoice();
+
+        let computerChoiceUpper = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+        let playerChoiceUpper = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+
+        let result = playRound(computerChoice, playerChoice);
+        console.log(`Round ${i+1} of 5.\n`)
+        
+        if (result === 0){
+            playerScore += 1;
+            computerScore += 1;
+            console.log(`Tie! ${playerChoiceUpper} ties with ${computerChoiceUpper}`);
+        }
+        else if (result === 1){
+            playerScore += 1;
+            console.log(`You Win! ${playerChoiceUpper} beats ${computerChoiceUpper}!`);
+        }
+        else{
+            computerScore += 1;
+            console.log(`You Lose! ${computerChoiceUpper} beats ${playerChoiceUpper}!`);
+        }
+    }
+
+    console.log(`The score is: \n Player - ${playerScore} \n Computer - ${computerScore}`);
+}
+
 function getComputerChoice(){
     let choice = '';
     let number = Math.floor(Math.random() * 3) + 1;
